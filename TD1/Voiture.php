5f21ee29e27f742376204cfeab68da8d0e7b91bf
Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-05 18:04:28 +0200

    Fin Exo 8

--- TD1/Voiture.php
+++ TD1/Voiture.php
@@ -1,48 +1,48 @@
 <?php
 class Voiture {
 
-    private $immatriculation;
-    private $marque;
-    private $couleur;
-    private $nbSieges; // Nombre de places assises
+    private string $immatriculation;
+    private string $marque;
+    private string $couleur;
+    private int $nbSieges; // Nombre de places assises
 
     // un getter
-    public function getMarque() {
+    public function getMarque() : string {
         return $this->marque;
     }
 
     // un setter
-    public function setMarque($marque) {
+    public function setMarque(string $marque) {
         $this->marque = $marque;
     }
 
-    public function getImmatriculation()
+    public function getImmatriculation() : string
     {
         return $this->immatriculation;
     }
 
-    public function setImmatriculation($immatriculation)
+    public function setImmatriculation(string $immatriculation)
     {
         $immatriculation =  substr($immatriculation, 0, 8);
         $this->immatriculation = $immatriculation;
     }
 
-    public function getCouleur()
+    public function getCouleur() : string
     {
         return $this->couleur;
     }
 
-    public function setCouleur($couleur)
+    public function setCouleur(string $couleur)
     {
         $this->couleur = $couleur;
     }
 
-    public function getNbSieges()
+    public function getNbSieges() : int
     {
         return $this->nbSieges;
     }
 
-    public function setNbSieges($nbSieges)
+    public function setNbSieges(int $nbSieges)
     {
         $this->nbSieges = $nbSieges;
     }
@@ -51,10 +51,10 @@ class Voiture {
 
     // un constructeur
     public function __construct(
-        $immatriculation,
-        $marque,
-        $couleur,
-        $nbSieges
+        string $immatriculation,
+        string $marque,
+        string $couleur,
+        int $nbSieges
     ) {
         $this->immatriculation = substr($immatriculation, 0, 8);
         $this->marque = $marque;
@@ -63,12 +63,12 @@ class Voiture {
     }
 
     // Pour pouvoir convertir un objet en chaîne de caractères
-    public function __toString() {
+    public function __toString() : string {
         // À compléter dans le prochain exercice
-        return 'Voiture marque : ' .$this->marque .
-            '<br> Couleur : ' .$this->couleur .
-            '<br> Immatriculation : ' .$this->immatriculation .
-            '<br> Nombre de places : ' .$this->nbSieges;
+        return 'Voiture marque : ' .$this->getMarque() .
+            '<br> Couleur : ' .$this->getCouleur() .
+            '<br> Immatriculation : ' .$this->getImmatriculation() .
+            '<br> Nombre de places : ' .$this->getNbSieges();
     }
 
 }
---
+++ TD1/testVoiture.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <?php
+    require_once("Voiture.php");
+    $voiture1 = new Voiture("EB2139713213412", "Mercedes", "Grise", 4);
+    echo $voiture1;
+    ?>
+    </body>
+</html>
