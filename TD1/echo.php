53b61a7c5dc62164b7e4d287eb2fccff68fa1ae6
Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-05 16:51:30 +0200

    fixed

--- TD1/echo.php
+++ TD1/echo.php
@@ -29,7 +29,7 @@ echo "Voiture $immatriculation de marque $marque (Couleur $couleur, $nbSieges si
 
 Exo 5.2
 */
-/*$voiture = [
+$voiture = [
         'marque' => 'Dacia',
     'couleur' => 'rouge',
     'immatriculation' => 'ZA-938-03',
@@ -40,15 +40,31 @@ Exo 5.2
 foreach ($voiture as $cle => $valeur){
     echo $cle ." : ".$valeur;
     echo "<br>";
-}*/
+}
 
 //echo "Voiture $voiture[immatriculation] de marque $voiture[marque] (Couleur $voiture[couleur], $voiture[nbSieges] sièges)";
 
 //var_dump($voiture);
 
 //Exo 5.4
-$voitures = [
+$voiture2 = [
+    'marque' => 'BMW',
+    'couleur' => 'noir',
+    'immatriculation' => 'ED-558-33',
+    'nbSieges' => 5
+];
 
+$voiture3 = [
+    'marque' => 'Renault',
+    'couleur' => 'blanc',
+    'immatriculation' => 'YV-908-19',
+    'nbSieges' => 5
+];
+
+$voitures = [
+        1 => $voiture,
+    2 => $voiture2,
+    3 => $voiture3
 ];
 
 //var_dump($voitures);
@@ -57,7 +73,8 @@ if(empty($voitures))
     echo "Il n’y a aucune voiture.";
 else
     foreach ($voitures as $cle => $valeur){
-        echo "<ul><li> $valeur </ul></li>";
+        foreach ($valeur as $cle2 => $valeur2)
+        echo "<ul><li> $valeur2 </ul></li>";
     }
 
 
