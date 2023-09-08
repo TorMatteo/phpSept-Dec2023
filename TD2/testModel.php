ec8aac930d5092810a21f2e92a6bd305f9392a0f
Tordeux Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-09 00:14:11 +0200

    exo 1.3 fini

--- TD2/testModel.php
+++ TD2/testModel.php
@@ -1,6 +1,5 @@
 <?php
 require_once "Model.php";
-
 // On affiche un attribut de PDO pour vérifier  que la connexion est bien établie.
 // Cela renvoie par ex. "webinfo.iutmontp.univ-montp2.fr via TCP/IP"
 // mais surtout pas de message d'erreur
