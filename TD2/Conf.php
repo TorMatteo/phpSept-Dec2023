846dba2e5e624392f89f3f61577200e33fd59e84
Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-11 11:44:19 +0200

    TD2 exo5

--- TD2/Conf.php
+++ TD2/Conf.php
@@ -7,13 +7,13 @@ class Conf {
         //
         // ou webinfo.iutmontp.univ-montp2.fr
         // pour accéder à webinfo depuis l'extérieur
-        'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
+        'hostname' => 'webinfo',
         // A l'IUT, vous avez une BDD nommee comme votre login
         // Sur votre machine, vous devrez creer une BDD
         'database' => 'tordeuxm',
         // À l'IUT, le port de MySQL est particulier : 3316
         // Ailleurs, on utilise le port par défaut : 3306
-        'port' => '3306',
+        'port' => '3316',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
         'login' => 'tordeuxm',
