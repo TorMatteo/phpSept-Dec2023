8af1d9276e6479f6acef1ef97b783cb9c3622557
Matteo <matteo.tordeux@etu.umontpellier.fr>
2023-09-12 14:51:49 +0200

    TD2 exo6

---
+++ TD2/lireVoiture.php
@@ -0,0 +1,19 @@
+<?php
+require_once("Model.php");
+require_once("Voiture.php");
+
+$pdoStatement = Model::getPdo()->query('SELECT * FROM voiture');
+$voitureFormatTableau = $pdoStatement->fetch();
+
+var_dump($voitureFormatTableau);
+
+$voiture = new Voiture($voitureFormatTableau[0], $voitureFormatTableau[1], $voitureFormatTableau[2], $voitureFormatTableau[3]);
+
+echo '<br>' . $voiture;
+
+foreach($pdoStatement as $voitureFormatTableau){
+    var_dump($voitureFormatTableau);
+}
+
+
+?>
